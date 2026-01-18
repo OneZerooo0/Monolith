cabddbec9614ea0625b8925eeba4f50d12dc2caf
Onezero0 <onezero0@outlook.com>
2026-01-17 19:45:04 -0500

    spent cartridge auto timeddespawn

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -40,6 +40,7 @@ using Robust.Shared.Random;
 using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
+using Robust.Shared.Spawners; // Mono
 
 namespace Content.Shared.Weapons.Ranged.Systems;
 
@@ -613,6 +614,12 @@ public abstract partial class SharedGunSystem : EntitySystem
 
         if (cartridge.DeleteOnSpawn) // Mono - No need to update appearance if cartridge is getting deleted anyways
             return;
+
+        if (cartridge.AutoTimedDespawn != 0 && !TryComp(uid, out TimedDespawnComponent? timedDespawnComponent)) // Mono - Used for lag reduction (lowered number of entities during gunfights)
+        {
+            AddComp<TimedDespawnComponent>(uid).Lifetime = cartridge.AutoTimedDespawn;
+        }
+        // End mono
 
         Appearance.SetData(uid, AmmoVisuals.Spent, spent);
     }
