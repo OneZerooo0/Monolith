b0f5e2c3494f879dc892fc61b8fe44255afb613e
Onezero0 <onezero0@outlook.com>
2026-03-06 16:03:54 -0500

    phase drive renaming and mechanic functions

--- Content.Server/Salvage/SalvageSystem.ExpeditionConsole.cs
+++ Content.Server/Salvage/SalvageSystem.ExpeditionConsole.cs
@@ -21,7 +21,7 @@ public sealed partial class SalvageSystem
 {
     [ValidatePrototypeId<EntityPrototype>]
     public const string CoordinatesDisk = "CoordinatesDisk";
-    private const float ShuttleFTLRange = 256f;
+    private const float ShuttleFTLRange = 50f;
     private const float ShuttleFTLMassThreshold = 100f;
 
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
