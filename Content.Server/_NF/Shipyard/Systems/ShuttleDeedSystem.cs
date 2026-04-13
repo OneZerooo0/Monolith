aca9a2bdbddb5139488af6f71ecf7c4a9d509d62
Onezero0 <onezero0@outlook.com>
2026-04-13 16:36:27 -0400

    unbreak:
    e

--- Content.Server/_NF/Shipyard/Systems/ShuttleDeedSystem.cs
+++ Content.Server/_NF/Shipyard/Systems/ShuttleDeedSystem.cs
@@ -43,22 +43,15 @@ public sealed partial class ShuttleDeedSystem : EntitySystem
     		var remainingTime = voucher.NextBuyAt - _timing.CurTime;
 
 			if (voucher.DestroyOnEmpty == true)
-        	{
             	args.PushMarkup(Loc.GetString("voucher-current-redemptions", ("count", voucher.RedemptionsLeft)));
-        	}
 			else
-			{
             	args.PushMarkup(Loc.GetString("voucher-infinite-redemptions"));
-        	}
 
 			if (remainingTime >= TimeSpan.FromSeconds(60))
-        	{
             	args.PushMarkup(Loc.GetString("voucher-current-cooldown-minutes", ("cooldown", remainingTime.TotalMinutes)));
-        	}
-			else if (remainingTime >= TimeSpan.FromSeconds(0))
-        	{
-            	args.PushMarkup(Loc.GetString("voucher-current-cooldown-seconds", ("cooldown", remainingTime.TotalSeconds)));
-        	}
+        	else if (remainingTime >= TimeSpan.FromSeconds(0))
+        		args.PushMarkup(Loc.GetString("voucher-current-cooldown-seconds", ("cooldown", remainingTime.TotalSeconds)));
+
 		}
     }
 }
